45867fb9362d492e8430100456698269a8a8a410
Meyappan Subbaiah <meysubb@gmail.com>
2018-02-05 17:35:48 -0600

    clean scraper and network calcs for missing listed_count value

--- Assignment_1/extract_tweets_api.py
+++ Assignment_1/extract_tweets_api.py
@@ -3,7 +3,7 @@ import tokens_twitt
 import pandas as pd
 from time import sleep
 
-columns = ['screen_name','text', 'name']
+columns = ['screen_name','text', 'name','listed_count']
 df = pd.DataFrame(columns=columns)
 
 api = twitter.Api(consumer_key=tokens_twitt.CONSUMER_KEY,
@@ -17,14 +17,18 @@ while(count < 5000):
   tweets = api.GetSearch(term='winter olympics', since=2018-1-15, count=100)
   for idx,line in enumerate(tweets):
     s = twitter.models.Status.AsDict(line)
-    df.loc[count+idx] = [s['user']['screen_name'],s['text'],s['user']['name']]
+    try:
+        lc = s['user']['listed_count']
+    except:
+        lc = 0
+    df.loc[count+idx] = [s['user']['screen_name'],s['text'],s['user']['name'],lc]
 
-  count += 100
+  count = len(df)
   print "Tweets processed: " + str(count)
   if(count<18000):
-      sleep(1)
+      sleep(60)
   else:
-      sleep(200)
+      sleep(2000)
 
 
 df.to_csv("raw_tweets.csv",index=False,encoding='utf-8')
